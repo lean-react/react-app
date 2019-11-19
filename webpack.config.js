05a091516d3102047d31f0e9011668436a49f1b7
Micha Alt <mail@lean-stack.rocks>
2019-11-19 20:18:35 +0100

    And let webpack handle the html.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,11 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   mode: 'development',
-  entry: './src/main.js'
+  entry: './src/main.js',
+
+  // Plugins
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' })
+  ]
 }
