63e7ae2f00d1e39df415b90bc024a2ec7497d07f
Micha Alt <mail@lean-stack.rocks>
2019-11-19 21:29:47 +0100

    Let webpack handle the styles.

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Styles
+import './styles/base.css';
+
 const appElt = <h2>Webpack Toolchain</h2>;
 ReactDOM.render(appElt, document.getElementById('app'));
