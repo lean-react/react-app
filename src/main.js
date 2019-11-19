730de0a1ef9c357a65efc24d11baf9173e54ac2b
Micha Alt <mail@lean-stack.rocks>
2019-11-19 19:54:06 +0100

    First Reactjs App. Let webpack handle the bundling

--- src/main.js
+++ src/main.js
@@ -1,2 +1,6 @@
 
-console.log('App started', new Date().toLocaleString());
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const appElt = React.createElement('h2', {}, 'Webpack Toolchain');
+ReactDOM.render(appElt, document.getElementById('app'));
